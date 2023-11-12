9e037be547b7b822cacefdbeb052393095bb209a
MarkSuttirak <93504873+suttirakcha@users.noreply.github.com>
2023-11-12 22:42:39 +0700

    Commit Codes

--- dashproj/webtemplate/models.py
+++ dashproj/webtemplate/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Topbar(models.Model):
     text = models.CharField(max_length=255)
     enable_topbar = models.BooleanField(default=False)
+
+class HeaderMenu(models.Model):
+    text = models.CharField(max_length=255)
+    link = models.SlugField()
